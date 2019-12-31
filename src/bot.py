a72cef498eac3545022cd56e4594b47a95907bd6
Anthony Hagouel <anthony.hagouel@gmail.com>
2019-12-30 22:44:17 -0800

    removed need for separate bash script to call fswebcam, now called directly in bot.py. Also added console outputs and did some general cleanup.

--- src/bot.py
+++ src/bot.py
@@ -3,41 +3,50 @@ from __future__ import absolute_import, print_function
 from gpiozero import MotionSensor
 from datetime import datetime 
 import tweepy
-import requests as r
-import os, sys, subprocess
+import subprocess
 
-#TODO: Add console output.
+# pickup sensor on RaspberryPi
+pir = MotionSensor(4)
+print("PIR sensor using " + str(pir.pin)) 
 
+# TODO: Put  keys into a file and read so this becomes generic
 # Complete OAuth Authentication for Twitter Bot
-
-# <<insert auth keys!>> TODO: Put your keys into a file and read so this is generic
-
+consumer_key=''
+consumer_secret=''
+access_token=''
+access_token_secret=''
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-# initialize API
+#Initialize API
+print("Initializing Twitter API with provided credentials")
 api = tweepy.API(auth)
-print('posting as ' + api.me().name)
+print('Success! Will be tweeting as ' + api.me().name)
 
-#TODO: Refactor into an actual program instead of this terrible script.
+#Function to tweet picture at filepath f
 def tweet_picture(f):
     ids = []
     media_response = api.media_upload(f)
     ids.append(media_response.media_id_string)
     api.update_status('WOOF!', media_ids = ids)
 
-# Motion Sensor loop
-pir = MotionSensor(4)
-filepath = '/home/pi/Development/MauiBot4000/media/img'
+#Function to take picture with webcam and save with timestamp to default location. Returns picture location.
+def take_picture(file_path = '/home/pi/Development/MauiBot4000/media/img', t = datetime.now().strftime("%Y-%m-%d:%T")):
+    command = "fswebcam -S 12 -r 1280x720 --no-banner " + file_path + t + '.jpg'
+    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+    return file_path + t + '.jpg'
+
+#Sensor as a loop + printing for fun :P 
 while True:
+    print('Waiting for motion...')
     pir.wait_for_motion()
-    print('Motion Detected')
-    print('Taking a picture of the sucker!')
-    #TODO: simplify by having BASH output the location of the file.
-    now = datetime.now().strftime("%Y-%m-%d:%T")
-    os.system('./webcam.sh ' + now)
-    picture_path = filepath + now + '.jpg'
-    tweet_picture(picture_path)
+    print('Motion Detected, capturing the sucker!')
+    #TODO: Surround in try/catch to restart loop if picture isn't taken.
+    picture = take_picture()
+    print('We got it! Tweeting out ' + picture +  ' to the world...', end='')
+    tweet_picture(picture)
+    print('Success!')
+    print('Waiting for things to calm down...', end = '')
     pir.wait_for_no_motion()
-    print('No motion detected')
+    print('No motion detected!')
     
