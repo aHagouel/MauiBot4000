0d7820f096e07b5bebd06e7a4dac1f6fd749b627
Anthony Hagouel <anthony.hagouel@gmail.com>
2020-01-08 23:02:52 -0800

    added switch & logic to stop capturing when switch is off so program can run but capturing can stop

--- src/bot.py
+++ src/bot.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 from __future__ import absolute_import, print_function
 from gpiozero import MotionSensor
-from datetime import datetime 
+import gpio
+from datetime import datetime
 import tweepy
 import time 
 import subprocess
 
 # pickup sensor on RaspberryPi
 pir = MotionSensor(4)
-print("PIR sensor using " + str(pir.pin)) 
+print('PIR sensor using ' + str(pir.pin))
+switch_pin = 27
+print('Idle program with pin ' + str(switch_pin))
 
 # TODO: Put  keys into a file and read so this becomes generic
 # Complete OAuth Authentication for Twitter Bot
@@ -23,7 +26,7 @@ auth.set_access_token(access_token, access_token_secret)
 print("Initializing Twitter API with provided credentials")
 api = tweepy.API(auth)
 print('Success! Will be tweeting as ' + api.me().name)
-
+# 
 #Function to tweet picture at filepath f
 def tweet_picture(f):
     ids = []
@@ -37,18 +40,26 @@ def take_picture(file_path = '/home/pi/Development/MauiBot4000/media/img', t = d
     process = subprocess.call(command.split(), stdout=subprocess.PIPE)
     return file_path + t + '.jpg'
 
-#Sensor as a loop + printing for fun :P 
+#Sensor as a loop + printing for fun :P
 while True:
-    print('Waiting for motion...')
-    pir.wait_for_motion()
-    print('Motion Detected, capturing the sucker!')
-    #TODO: Surround in try/catch to restart loop if picture isn't taken.
-    picture = take_picture()
-    print('We got it! Tweeting out ' + picture +  ' to the world...', end='')
-    tweet_picture(picture)
-    print('Success!')
-    print('Waiting for things to calm down...', end = '')
-    time.sleep(10)
-    pir.wait_for_no_motion()
-    print('No motion detected!')
-    
+	if(gpio.input(switch_pin) == 1):
+		print('Waiting for motion...')
+		pir.wait_for_motion()
+		#if switch was disconnected while waiting for motion
+		if(gpio.input(switch_pin) == 1):
+			print('Motion Detected, capturing the sucker!')
+			picture = take_picture()
+			print('We got it! Tweeting out ' + picture +  ' to the world...', end='')
+			tweet_picture(picture)
+			print('Success!')
+			print('Waiting for things to calm down...', end = '')
+			time.sleep(10)
+			pir.wait_for_no_motion()
+			print('No motion detected!')
+		else: 
+			continue
+	else:
+		print('Not capturing until switch is on.')
+		time.sleep(15)    
+
+
